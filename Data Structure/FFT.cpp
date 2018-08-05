478b6e682aaf4a380b36b4508481dbe94e7cdd90
juniorandrade1 <junior.andrade.cco@gmail.com>
2018-08-05 17:39:52 -0300

    Fix fft

--- Data Structure/FFT.cpp
+++ Data Structure/FFT.cpp
@@ -1,58 +1,91 @@
-#define PI acos(-1.0)
+namespace FFT {
 
-struct Complex {
-  double a, b; // a + bi
-  Complex(){
-    a = 0.0;
-    b = 0.0;
+  struct ComplexType {
+    double a, b;
+    ComplexType() {
+      a = b = 0;
+    }
+    ComplexType(double _a, double _b) {
+      a = _a;
+      b = _b;
+    }
+    ComplexType(double _a) {
+      a = _a;
+      b = 0;
+    }
+    ComplexType operator + (ComplexType o) { return ComplexType(a + o.a, b + o.b); }
+    ComplexType operator - (ComplexType o) { return ComplexType(a - o.a, b - o.b); }
+    ComplexType operator * (ComplexType o) { return ComplexType(a * o.a - b * o.b, a * o.b + b * o.a); }
+    void operator *= (ComplexType o) { 
+      double na = a * o.a - b * o.b;
+      double nb = a * o.b + b * o.a;
+      a = na;
+      b = nb;
+    }
+    void operator /= (double x) {
+      a /= x;
+      b /= x;
+    }
+    double real() { return a; }
+    double imag() { return b; }
   };
-  Complex(double _a, double _b) {
-    a = _a;
-    b = _b;
-  }
-  Complex operator+(const Complex &o)const {
-    return Complex(o.a + a, b + o.b);
+
+  const int M = 21;
+  const int N = (1 << M);
+  
+  int pre[N][M];
+
+  int reverse(int num, int lg_n) {
+    if(pre[num][lg_n]) return pre[num][lg_n] - 1;
+    int res = 0;
+    for (int i = 0; i < lg_n; i++) {
+      if (num & (1 << i))
+        res |= 1 << (lg_n - 1 - i);
+    }
+    pre[num][lg_n] = res + 1;
+    return res;
   }
 
-  Complex operator/(double v) const {
-    return Complex(a / v, b / v);
-  }
-  Complex operator*(const Complex &o)const {
-    return Complex(a * o.a - b * o.b, a * o.b + b * o.a);
-  }
-
-  Complex operator-(const Complex &o) const {
-    return Complex(a - o.a, b - o.b);
-  }
-};
-
-int rev[N];
-Complex wlen_pw[N];
- 
-void fft (Complex a[], int n, bool invert) {
-  for (int i = 0; i < n; ++i) if (i < rev[i]) swap (a[i], a[rev[i]]);
-  for (int len = 2; len <= n; len <<= 1) {
-    double ang = 2 * PI / len * (invert ? -1 : +1);
-    int len2 = len >> 1;
-    Complex wlen (cos(ang), sin(ang));
-    wlen_pw[0] = Complex(1, 0);
-    for (int i = 1; i < len2; ++i) wlen_pw[i] = wlen_pw[i - 1] * wlen;
-    for (int i=0; i<n; i+=len) {
-      Complex t, *pu = a+i, *pv = a+i+len2,  *pu_end = a+i+len2, *pw = wlen_pw;
-      for (; pu!=pu_end; ++pu, ++pv, ++pw) {
-        t = *pv * *pw;
-        *pv = *pu - t;
-        *pu = *pu + t;
+  void fft(vector<ComplexType> & a, bool invert) {
+    int n = a.size();
+    int lg_n = 0;
+    while ((1 << lg_n) < n) lg_n++;
+    for (int i = 0; i < n; i++) {
+      if (i < reverse(i, lg_n)) swap(a[i], a[reverse(i, lg_n)]);
+    }
+    for (int len = 2; len <= n; len <<= 1) {
+      double ang = 2 * PI / len * (invert ? -1 : 1);
+      ComplexType wlen(cos(ang), sin(ang));
+      for (int i = 0; i < n; i += len) {
+        ComplexType w(1);
+        for (int j = 0; j < len / 2; j++) {
+          ComplexType u = a[i+j], v = a[i+j+len/2] * w;
+          a[i+j] = u + v;
+          a[i+j+len/2] = u - v;
+          w *= wlen;
+        }
       }
     }
+    if (invert) {
+      for (ComplexType & x : a) x /= n;
+    }
   }
-  if (invert) for (int i=0; i<n; ++i) a[i] = a[i] / n;
-}
- 
-void calc_rev (int n, int log_n) {
-  for (int i = 0; i < n; ++i) {
-    rev[i] = 0;
-    for (int j = 0; j < log_n; ++j)
-      if (i & (1 << j)) rev[i] |= 1 << (log_n - 1 - j);
+
+  inline vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<ComplexType> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++) fa[i] *= fb[i];
+    fft(fa, true);
+    vector<int> result(n);
+    for (int i = 0; i < n; i++) {
+      ll x = fa[i].real() + 0.5;
+      result[i] = x;
+    }
+    return result;
   }
-}
+};
